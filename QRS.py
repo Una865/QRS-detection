8a1d3c14376076b526f46be29acfb91b35432703
Una865 <62244939+Una865@users.noreply.github.com>
2022-07-25 11:16:46 +0200

    Update QRS.py

--- QRS.py
+++ QRS.py
@@ -1,13 +1,15 @@
 import os
 import sys
 from util.QRS_util import*
+import pywt
+import wfdb
 
 '''
 QRS detection demo 
 @author: Kemeng Chen: kemengchen@email.arizona.edu
 '''
 
-def QRS_test(ecg)raw):
+def QRS_test():
 	'''
 	QRS detection on file_name
 	assuming 360 Hz sampling rate, may not work with very low sampling rate signal
@@ -19,9 +21,10 @@ def QRS_test(ecg)raw):
 	#if not os.path.isfile(file_path):
 		#raise AssertionError(file_path, 'not exists')
 	#ecg=read_ecg(file_path)
-	R_peaks, S_pint, Q_point=EKG_QRS_detect(ecg, fs, False, True)
+	signals, fields= wfdb.rdsamp('mitdb/200')
+	
+	R_peaks, S_pint, Q_point=EKG_QRS_detect(signals[:,0], fs, False, True)
 
 if __name__ == '__main__':
-	if len(sys.argv)<2:
-		raise ValueError('No file name specified')
-	QRS_test(sys.argv[1])
+	
+	QRS_test()

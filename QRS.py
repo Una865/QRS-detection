22546c5c105c5360c2d6c869663b75047485cbc4
Una865 <62244939+Una865@users.noreply.github.com>
2022-07-25 11:14:32 +0200

    Update QRS.py

--- QRS.py
+++ QRS.py
@@ -7,7 +7,7 @@ QRS detection demo
 @author: Kemeng Chen: kemengchen@email.arizona.edu
 '''
 
-def QRS_test(file_name):
+def QRS_test(ecg)raw):
 	'''
 	QRS detection on file_name
 	assuming 360 Hz sampling rate, may not work with very low sampling rate signal
@@ -15,10 +15,10 @@ def QRS_test(file_name):
 		file_name: file containing ecg data in one column
 	'''
 	fs=360
-	file_path=os.path.join(os.getcwd(), 'data', file_name)
-	if not os.path.isfile(file_path):
-		raise AssertionError(file_path, 'not exists')
-	ecg=read_ecg(file_path)
+	#file_path=os.path.join(os.getcwd(), 'data', file_name)
+	#if not os.path.isfile(file_path):
+		#raise AssertionError(file_path, 'not exists')
+	#ecg=read_ecg(file_path)
 	R_peaks, S_pint, Q_point=EKG_QRS_detect(ecg, fs, False, True)
 
 if __name__ == '__main__':
